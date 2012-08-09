24693d20e0ea95d62669636273df4e151eae03b6
max10 <max_10@gmx.de>
2012-08-09 18:15:46 +0200

    update enigma2-pli-nightly.mk

--- tdt/cvs/cdk/make/enigma2-pli-nightly.mk
+++ tdt/cvs/cdk/make/enigma2-pli-nightly.mk
@@ -7,32 +7,30 @@ $(DEPDIR)/enigma2-pli-nightly.do_prepare:
 	REPO="git://gitorious.org/open-duckbox-project-sh4/guigit.git"; \
 	rm -rf $(appsdir)/enigma2-nightly; \
 	rm -rf $(appsdir)/enigma2-nightly.org; \
-	rm -rf $(appsdir)/enigma2-nightly.newest; \
-	rm -rf $(appsdir)/enigma2-nightly.patched; \
 	clear; \
 	echo "Media Framwork: $(MEDIAFW)"; \
 	echo "Choose between the following revisions:"; \
 	echo " 0) Newest (Can fail due to outdated patch)"; \
 	echo "---- REVISIONS ----"; \
 	echo "1) Sat, 17 Mar 2012 19:51 - E2 OpenPli 945aeb939308b3652b56bc6c577853369d54a537"; \
-	echo "2) Sat, 18 May 2012 15:26  - E2 OpenPli 839e96b79600aba73f743fd39628f32bc1628f4c"; \
+	echo "2) Sat, 18 May 2012 15:26 - E2 OpenPli 839e96b79600aba73f743fd39628f32bc1628f4c"; \
+	echo "3) Sat,  8 Aug 2012 19:30 - E2 OpenPli a3b233a2545a5de7ed1b798137ee1da597856b9e"; \
 	read -p "Select: "; \
 	echo "Selection: " $$REPLY; \
 	[ "$$REPLY" == "0" ] && DIFF="0" && HEAD="experimental"; \
 	[ "$$REPLY" == "1" ] && DIFF="1" && REVISION="945aeb939308b3652b56bc6c577853369d54a537" && REPO="git://openpli.git.sourceforge.net/gitroot/openpli/enigma2"; \
 	[ "$$REPLY" == "2" ] && DIFF="2" && REVISION="839e96b79600aba73f743fd39628f32bc1628f4c" && REPO="git://openpli.git.sourceforge.net/gitroot/openpli/enigma2"; \
+	[ "$$REPLY" == "3" ] && DIFF="3" && REVISION="a3b233a2545a5de7ed1b798137ee1da597856b9e" && REPO="git://openpli.git.sourceforge.net/gitroot/openpli/enigma2"; \
 	echo "Revision: " $$REVISION; \
 	[ -d "$(appsdir)/enigma2-nightly" ] && \
 	git pull $(appsdir)/enigma2-nightly $$HEAD;\
 	[ -d "$(appsdir)/enigma2-nightly" ] || \
 	git clone -b $$HEAD $$REPO $(appsdir)/enigma2-nightly; \
-	cp -ra $(appsdir)/enigma2-nightly $(appsdir)/enigma2-nightly.newest; \
 	[ "$$REVISION" == "" ] || (cd $(appsdir)/enigma2-nightly; git checkout "$$REVISION"; cd "$(buildprefix)";); \
 	cp -ra $(appsdir)/enigma2-nightly $(appsdir)/enigma2-nightly.org; \
 	cd $(appsdir)/enigma2-nightly && patch -p1 < "../../cdk/Patches/enigma2-pli-nightly.$$DIFF.diff"; \
 	cd $(appsdir)/enigma2-nightly && patch -p1 < "../../cdk/Patches/enigma2-pli-nightly.$$DIFF.$(MEDIAFW).diff"; \
-	[ "$(EXTERNALLCD_DEP)" == "" ] || (cd $(appsdir)/enigma2-nightly && patch -p1 < "../../cdk/Patches/enigma2-pli-nightly.$$DIFF.graphlcd.diff" ); \
-	cp -ra $(appsdir)/enigma2-nightly $(appsdir)/enigma2-nightly.patched
+	[ "$(EXTERNALLCD_DEP)" == "" ] || (cd $(appsdir)/enigma2-nightly && patch -p1 < "../../cdk/Patches/enigma2-pli-nightly.$$DIFF.graphlcd.diff" )
 	touch $@
 
 $(appsdir)/enigma2-pli-nightly/config.status: bootstrap freetype expat fontconfig libpng jpeg libgif libfribidi libid3tag libmad libsigc libreadline \
@@ -80,4 +78,3 @@ enigma2-pli-nightly-clean enigma2-pli-nightly-distclean:
 	rm -rf $(appsdir)/enigma2-nightly.newest
 	rm -rf $(appsdir)/enigma2-nightly.org
 	rm -rf $(appsdir)/enigma2-nightly.patched
-

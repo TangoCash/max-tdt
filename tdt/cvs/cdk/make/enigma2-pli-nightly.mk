7102ade208df37b0a30d43c1f369016f65404b0e
max10 <max_10@gmx.de>
2012-08-13 17:10:41 +0200

    update enigma2-pli-nightly.mk, add enigma2 pli eplayer3

--- tdt/cvs/cdk/make/enigma2-pli-nightly.mk
+++ tdt/cvs/cdk/make/enigma2-pli-nightly.mk
@@ -1,36 +1,46 @@
+#
 # tuxbox/enigma2
-
+#
 $(DEPDIR)/enigma2-pli-nightly.do_prepare:
 	REVISION=""; \
 	HEAD="master"; \
 	DIFF="0"; \
-	REPO="git://gitorious.org/open-duckbox-project-sh4/guigit.git"; \
+	REPO="git://openpli.git.sourceforge.net/gitroot/openpli/enigma2"; \
 	rm -rf $(appsdir)/enigma2-nightly; \
 	rm -rf $(appsdir)/enigma2-nightly.org; \
+	rm -rf $(appsdir)/enigma2-nightly.newest; \
+	rm -rf $(appsdir)/enigma2-nightly.patched; \
 	clear; \
+	echo ""; \
 	echo "Media Framwork: $(MEDIAFW)"; \
+	echo ""; \
 	echo "Choose between the following revisions:"; \
-	echo " 0) Newest (Can fail due to outdated patch)"; \
-	echo "---- REVISIONS ----"; \
-	echo "1) Sat, 17 Mar 2012 19:51 - E2 OpenPli 945aeb939308b3652b56bc6c577853369d54a537"; \
-	echo "2) Sat, 18 May 2012 15:26 - E2 OpenPli 839e96b79600aba73f743fd39628f32bc1628f4c"; \
-	echo "3) Sat,  8 Aug 2012 19:30 - E2 OpenPli a3b233a2545a5de7ed1b798137ee1da597856b9e"; \
+	echo "---------------------------------------"; \
+	echo "0) Newest (Can fail due to outdated patch)"; \
+	echo "1) Sat, 17 Mar 2012 19:51 - E2 OpenPli gstreamer 945aeb939308b3652b56bc6c577853369d54a537"; \
+	echo "2) Sat, 18 May 2012 15:26 - E2 OpenPli gstreamer 839e96b79600aba73f743fd39628f32bc1628f4c"; \
+	echo "3) Sat,  8 Aug 2012 19:30 - E2 OpenPli gstreamer a3b233a2545a5de7ed1b798137ee1da597856b9e"; \
+	echo "4) Sat,  8 Aug 2012 19:30 - E2 OpenPli eplayer3  a3b233a2545a5de7ed1b798137ee1da597856b9e"; \
+	echo "5) Sat, 11 Aug 2012 21:50 - E2 OpenPli eplayer3  b67f9debd524873e77e8a3e87c1e8ddee79b810d"; \
 	read -p "Select: "; \
 	echo "Selection: " $$REPLY; \
-	[ "$$REPLY" == "0" ] && DIFF="0" && HEAD="experimental"; \
-	[ "$$REPLY" == "1" ] && DIFF="1" && REVISION="945aeb939308b3652b56bc6c577853369d54a537" && REPO="git://openpli.git.sourceforge.net/gitroot/openpli/enigma2"; \
-	[ "$$REPLY" == "2" ] && DIFF="2" && REVISION="839e96b79600aba73f743fd39628f32bc1628f4c" && REPO="git://openpli.git.sourceforge.net/gitroot/openpli/enigma2"; \
-	[ "$$REPLY" == "3" ] && DIFF="3" && REVISION="a3b233a2545a5de7ed1b798137ee1da597856b9e" && REPO="git://openpli.git.sourceforge.net/gitroot/openpli/enigma2"; \
+	[ "$$REPLY" == "0" ] && DIFF="0"; \
+	[ "$$REPLY" == "1" ] && DIFF="1" && REVISION="945aeb939308b3652b56bc6c577853369d54a537"; \
+	[ "$$REPLY" == "2" ] && DIFF="2" && REVISION="839e96b79600aba73f743fd39628f32bc1628f4c"; \
+	[ "$$REPLY" == "3" ] && DIFF="3" && REVISION="a3b233a2545a5de7ed1b798137ee1da597856b9e"; \
+	[ "$$REPLY" == "4" ] && DIFF="4" && REVISION="a3b233a2545a5de7ed1b798137ee1da597856b9e"; \
+	[ "$$REPLY" == "5" ] && DIFF="5" && REVISION="b67f9debd524873e77e8a3e87c1e8ddee79b810d"; \
 	echo "Revision: " $$REVISION; \
 	[ -d "$(appsdir)/enigma2-nightly" ] && \
 	git pull $(appsdir)/enigma2-nightly $$HEAD;\
 	[ -d "$(appsdir)/enigma2-nightly" ] || \
 	git clone -b $$HEAD $$REPO $(appsdir)/enigma2-nightly; \
+	cp -ra $(appsdir)/enigma2-nightly $(appsdir)/enigma2-nightly.newest; \
 	[ "$$REVISION" == "" ] || (cd $(appsdir)/enigma2-nightly; git checkout "$$REVISION"; cd "$(buildprefix)";); \
 	cp -ra $(appsdir)/enigma2-nightly $(appsdir)/enigma2-nightly.org; \
 	cd $(appsdir)/enigma2-nightly && patch -p1 < "../../cdk/Patches/enigma2-pli-nightly.$$DIFF.diff"; \
-	cd $(appsdir)/enigma2-nightly && patch -p1 < "../../cdk/Patches/enigma2-pli-nightly.$$DIFF.$(MEDIAFW).diff"; \
-	[ "$(EXTERNALLCD_DEP)" == "" ] || (cd $(appsdir)/enigma2-nightly && patch -p1 < "../../cdk/Patches/enigma2-pli-nightly.$$DIFF.graphlcd.diff" )
+	[ "$(EXTERNALLCD_DEP)" == "" ] || (cd $(appsdir)/enigma2-nightly && patch -p1 < "../../cdk/Patches/enigma2-pli-nightly.$$DIFF.graphlcd.diff" ); \
+	cp -ra $(appsdir)/enigma2-nightly $(appsdir)/enigma2-nightly.patched
 	touch $@
 
 $(appsdir)/enigma2-pli-nightly/config.status: bootstrap freetype expat fontconfig libpng jpeg libgif libfribidi libid3tag libmad libsigc libreadline \

1c0872a97e4e18a0d8439edf3577ca595e41b93d
max10 <max_10@gmx.de>
2012-09-12 15:13:54 +0200

    fix libass

--- tdt/cvs/cdk/make/contrib-libs.mk
+++ tdt/cvs/cdk/make/contrib-libs.mk
@@ -998,7 +998,7 @@ $(DEPDIR)/%ffmpeg: $(DEPDIR)/ffmpeg.do_compile
 #
 # libass
 #
-$(DEPDIR)/libass.do_prepare: bootstrap freetype @DEPENDS_libass@
+$(DEPDIR)/libass.do_prepare: bootstrap freetype libfribidi @DEPENDS_libass@
 	@PREPARE_libass@
 	touch $@
 

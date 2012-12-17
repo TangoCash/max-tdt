b92dc4a028bb9e43fcfb6aa7662b00d8c6ba5e12
max10 <max_10@gmx.de>
2012-12-17 17:10:13 +0100

    cosmetics in bootstrap.mk

--- tdt/cvs/cdk/make/bootstrap.mk
+++ tdt/cvs/cdk/make/bootstrap.mk
@@ -618,8 +618,7 @@ $(CROSS_GCC_RPM) $(CROSS_CPP_RPM) $(CROSS_G++_RPM) $(CROSS_PROTOIZE_RPM) $(CROSS
 		$(archivedir)/$(STLINUX)-$(subst cross-sh4-,cross-,$(CROSS_GCC))-$(CROSS_GCC_VERSION).src.rpm \
 		| $(archivedir)/$(STLINUX)-sh4-$(GLIBC)-$(GLIBC_VERSION).sh4.rpm \
 		$(archivedir)/$(STLINUX)-sh4-$(GLIBC_DEV)-$(GLIBC_VERSION).sh4.rpm \
-		$(if $(KERNELHEADERS),$(KERNELHEADERS)) \
-		kernel-headers
+		$(CROSS_GCC_KERNELHEADERS)
 	rpm $(DRPM) --nosignature --ignorearch --nodeps --force -Uhv \
 		--badreloc --relocate $(STM_RELOCATE)/devkit/sh4/target=$(targetprefix) $(word 1,$|) && \
 	rpm $(DRPM) --nosignature --ignorearch --nodeps --force -Uhv \

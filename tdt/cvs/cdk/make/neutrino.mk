a00902a454fc4d82a0063f11221f01d0e6ab2a32
max10 <max_10@gmx.de>
2012-12-21 17:50:49 +0100

    add Neutrino MARTII

--- tdt/cvs/cdk/make/neutrino.mk
+++ tdt/cvs/cdk/make/neutrino.mk
@@ -14,17 +14,123 @@ $(targetprefix)/var/etc/.version:
 #
 #
 #
-N_CPPFLAGS = -DNEW_LIBCURL -DFB_BLIT -DBGP_JPG
+N_CPPFLAGS = -DNEW_LIBCURL -DFB_BLIT -DBGP_JPG -I$(driverdir)/frontcontroller/aotom -I$(driverdir)/bpamem
 
-N_CONFIG_OPTS = --enable-silent-rules
+N_CONFIG_OPTS = --enable-silent-rules --enable-freesatepg
 
 if ENABLE_EXTERNALLCD
 N_CONFIG_OPTS += --enable-graphlcd
 endif
 
-if ENABLE_EPLAYER3
-N_CONFIG_OPTS += --enable-libeplayer3
-endif
+#
+# LIBSTB-HAL
+#
+$(DEPDIR)/libstb-hal.do_prepare:
+	rm -rf $(appsdir)/libstb-hal
+	rm -rf $(appsdir)/libstb-hal.org
+	[ -d "$(archivedir)/libstb-hal.git" ] && \
+	(cd $(archivedir)/libstb-hal.git; git pull ; git checkout HEAD; cd "$(buildprefix)";); \
+	[ -d "$(archivedir)/libstb-hal.git" ] || \
+	git clone git://gitorious.org/~martii/neutrino-hd/martiis-libstb-hal.git $(archivedir)/libstb-hal.git; \
+	cp -ra $(archivedir)/libstb-hal.git $(appsdir)/libstb-hal;\
+	cp -ra $(appsdir)/libstb-hal $(appsdir)/libstb-hal.org
+	touch $@
+
+$(appsdir)/libstb-hal/config.status: bootstrap
+	export PATH=$(hostprefix)/bin:$(PATH) && \
+	cd $(appsdir)/libstb-hal && \
+		ACLOCAL_FLAGS="-I $(hostprefix)/share/aclocal" ./autogen.sh && \
+		$(BUILDENV) \
+		./configure \
+			--host=$(target) \
+			--build=$(build) \
+			--prefix=/usr \
+			--with-boxtype=spark \
+			PKG_CONFIG=$(hostprefix)/bin/pkg-config \
+			PKG_CONFIG_PATH=$(targetprefix)/usr/lib/pkgconfig \
+			$(PLATFORM_CPPFLAGS) \
+			CPPFLAGS="$(CPPFLAGS) -DMARTII -I$(driverdir)/frontcontroller/aotom"
+
+$(DEPDIR)/libstb-hal.do_compile: $(appsdir)/libstb-hal/config.status
+	cd $(appsdir)/libstb-hal && \
+		$(MAKE)
+	touch $@
+
+$(DEPDIR)/libstb-hal: libstb-hal.do_prepare libstb-hal.do_compile
+	$(MAKE) -C $(appsdir)/libstb-hal install DESTDIR=$(targetprefix)
+	touch $@
+
+libstb-hal-clean:
+	rm -f $(DEPDIR)/libstb-hal
+	cd $(appsdir)/libstb-hal && \
+		$(MAKE) distclean
+
+libstb-hal-distclean:
+	rm -f $(DEPDIR)/libstb-hal
+	rm -f $(DEPDIR)/libstb-hal.do_compile
+	rm -f $(DEPDIR)/libstb-hal.do_prepare
+
+#
+# NEUTRINO MARTII
+#
+$(DEPDIR)/neutrino-hd.do_prepare:
+	rm -rf $(appsdir)/neutrino-hd
+	rm -rf $(appsdir)/neutrino-hd.org
+	[ -d "$(archivedir)/neutrino-hd.git" ] && \
+	(cd $(archivedir)/neutrino-hd.git; git pull ; git checkout HEAD; cd "$(buildprefix)";); \
+	[ -d "$(archivedir)/neutrino-hd.git" ] || \
+	git clone git://gitorious.org/~martii/neutrino-hd/martiis-neutrino-hd-tripledragon.git $(archivedir)/neutrino-hd.git; \
+	cp -ra $(archivedir)/neutrino-hd.git $(appsdir)/neutrino-hd; \
+	cp -ra $(appsdir)/neutrino-hd $(appsdir)/neutrino-hd.org
+	touch $@
+
+$(appsdir)/neutrino-hd/config.status: bootstrap $(EXTERNALLCD_DEP) libdvbsipp libfreetype libjpeg libpng libungif libid3tag libcurl libmad libvorbisidec libboost openssl libopenthreads sdparm libusb2 libalsa libstb-hal
+	export PATH=$(hostprefix)/bin:$(PATH) && \
+	cd $(appsdir)/neutrino-hd && \
+		$(BUILDENV) \
+		ACLOCAL_FLAGS="-I $(hostprefix)/share/aclocal" ./autogen.sh && \
+		./configure \
+			--host=$(target) \
+			$(N_CONFIG_OPTS) \
+			--with-tremor \
+			--with-libdir=/usr/lib \
+			--with-datadir=/share/tuxbox \
+			--with-fontdir=/share/fonts \
+			--with-configdir=/var/tuxbox/config \
+			--with-gamesdir=/var/tuxbox/games \
+			--with-plugindir=/var/tuxbox/plugins \
+			--with-boxtype=spark \
+			--with-stb-hal-includes=$(appsdir)/libstb-hal/include \
+			--with-stb-hal-build=$(appsdir)/libstb-hal \
+			PKG_CONFIG=$(hostprefix)/bin/pkg-config \
+			PKG_CONFIG_PATH=$(targetprefix)/usr/lib/pkgconfig \
+			$(PLATFORM_CPPFLAGS) \
+			CPPFLAGS="$(N_CPPFLAGS) -DMARTII"
+
+$(DEPDIR)/neutrino-hd.do_compile: $(appsdir)/neutrino-hd/config.status
+	cd $(appsdir)/neutrino-hd && \
+		$(MAKE) all
+	touch $@
+
+$(DEPDIR)/neutrino-hd: neutrino-hd.do_prepare neutrino-hd.do_compile
+	$(MAKE) -C $(appsdir)/neutrino-hd install DESTDIR=$(targetprefix) && \
+	make $(targetprefix)/var/etc/.version
+	$(target)-strip $(targetprefix)/usr/local/bin/neutrino
+	$(target)-strip $(targetprefix)/usr/local/bin/pzapit
+	$(target)-strip $(targetprefix)/usr/local/bin/sectionsdcontrol
+	$(target)-strip $(targetprefix)/usr/local/sbin/udpstreampes
+	touch $@
+
+neutrino-hd-clean:
+	rm -f $(DEPDIR)/neutrino-hd
+	cd $(appsdir)/neutrino-hd && \
+		$(MAKE) distclean
+
+neutrino-hd-distclean:
+	rm -f $(DEPDIR)/neutrino-hd
+	rm -f $(DEPDIR)/neutrino-hd.do_compile
+	rm -f $(DEPDIR)/neutrino-hd.do_prepare
+
 
 #
 # NEUTRINO TWIN
@@ -32,11 +138,11 @@ endif
 $(DEPDIR)/neutrino-twin.do_prepare:
 	rm -rf $(appsdir)/neutrino-twin
 	rm -rf $(appsdir)/neutrino-twin.org
-	[ -d "$(archivedir)/neutrino-hd.git" ] && \
-	(cd $(archivedir)/neutrino-hd.git; git pull ; git checkout HEAD; cd "$(buildprefix)";); \
-	[ -d "$(archivedir)/neutrino-hd.git" ] || \
-	git clone git://c00lstreamtech.de/cst-public-gui-neutrino.git $(archivedir)/neutrino-hd.git; \
-	cp -ra $(archivedir)/neutrino-hd.git $(appsdir)/neutrino-twin; \
+	[ -d "$(archivedir)/cst-public-gui-neutrino.git" ] && \
+	(cd $(archivedir)/cst-public-gui-neutrino.git; git pull ; git checkout HEAD; cd "$(buildprefix)";); \
+	[ -d "$(archivedir)/cst-public-gui-neutrino.git" ] || \
+	git clone git://c00lstreamtech.de/cst-public-gui-neutrino.git $(archivedir)/cst-public-gui-neutrino.git; \
+	cp -ra $(archivedir)/cst-public-gui-neutrino.git $(appsdir)/neutrino-twin; \
 	(cd $(appsdir)/neutrino-twin; git checkout dvbsi++; cd "$(buildprefix)";); \
 	rm -rf $(appsdir)/neutrino-twin/lib/libcoolstream/*.*
 	cp -ra $(appsdir)/neutrino-twin $(appsdir)/neutrino-twin.org

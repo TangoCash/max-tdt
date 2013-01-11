79d3eb7ee0c093d1a89d168327b5b02c3f69a0ef
BPanther <bpanther_ts@hotmail.com>
2013-01-11 15:31:55 +0100

    [neutrino] - flash script fix

--- tdt/flash/ufs913/scripts/prepare_root.sh
+++ tdt/flash/ufs913/scripts/prepare_root.sh
@@ -10,7 +10,7 @@ TMPFWDIR=$5
 cp -a $RELEASEDIR/* $TMPROOTDIR
 
 cd $TMPROOTDIR/dev/
-$TMPROOTDIR/etc/init.d/makedev start
+$TMPROOTDIR/var/etc/init.d/makedev start
 cd -
 
 mv $TMPROOTDIR/boot/uImage $TMPKERNELDIR/uImage
@@ -18,11 +18,7 @@ mv $TMPROOTDIR/boot/uImage $TMPKERNELDIR/uImage
 mv $TMPROOTDIR/boot/audio.elf $TMPFWDIR/audio.elf
 mv $TMPROOTDIR/boot/video.elf $TMPFWDIR/video.elf
 
-mv $TMPROOTDIR/boot/bootlogo.mvi $TMPROOTDIR/etc/bootlogo.mvi
-sed -i "s/\/boot\/bootlogo.mvi/\/etc\/bootlogo.mvi/g" $TMPROOTDIR/etc/init.d/rcS
-
 rm -f $TMPROOTDIR/boot/*
 
-echo "/dev/mtdblock8	/boot	jffs2	defaults	0	0" >> $TMPROOTDIR/etc/fstab
-#echo "/dev/mtdblock10	/root	jffs2	defaults	0	0" >> $TMPROOTDIR/etc/fstab
-
+echo "/dev/mtdblock8	/boot	jffs2	defaults	0	0" >> $TMPROOTDIR/var/etc/fstab
+#echo "/dev/mtdblock10	/swap	jffs2	defaults	0	0" >> $TMPROOTDIR/var/etc/fstab

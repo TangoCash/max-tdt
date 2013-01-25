ad37faa6b5281be2531068270e9affd24b5a7293
BPanther <bpanther_ts@hotmail.com>
2013-01-25 01:47:35 +0100

    small fix

--- tdt/cvs/cdk/make.sh
+++ tdt/cvs/cdk/make.sh
@@ -5,7 +5,7 @@ if [ "$1" == -h ] || [ "$1" == --help ]; then
  echo "Parameter 2: kernel (1-4)"
  echo "Parameter 3: debug (y/N)"
  echo "Parameter 4: player (1-2)"
- echo "Parameter 5: Media Framework (1-2)"
+ echo "Parameter 5: Media Framework (1-3)"
  echo "Parameter 6: External LCD support (1-2)"
  echo "Parameter 7: Graphic Framework (1-2)"
  exit

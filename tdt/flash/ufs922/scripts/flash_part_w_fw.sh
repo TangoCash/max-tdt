0d0b898c9d2d3fb51efb14a6c8f1a67ff16a3954
TangoCash <me@spamdot.com>
2013-01-28 18:32:43 +0100

    switched squashfs to gzip -> update kernel partitions and flash script for UFS922

--- tdt/flash/ufs922/scripts/flash_part_w_fw.sh
+++ tdt/flash/ufs922/scripts/flash_part_w_fw.sh
@@ -19,7 +19,7 @@ MKSQUASHFS=$CURDIR/../common/mksquashfs4.0
 SUMTOOL=$TUFSBOXDIR/host/bin/sumtool
 PAD=$CURDIR/../common/pad
 
-OUTFILE=$OUTDIR/update_w_fw.img
+OUTFILE=$OUTDIR/ufs922_`git describe`.img
 
 if [ ! -e $OUTDIR ]; then
   mkdir $OUTDIR
@@ -36,20 +36,20 @@ $PAD 0x1a0000 $CURDIR/uImage $CURDIR/mtd_kernel.pad.bin
 
 # --- ROOT ---
 # Create a squashfs partition for root
-# Size 0xa20000
-echo "MKSQUASHFS $TMPROOTDIR $CURDIR/mtd_root.bin -comp lzma -all-root"
-$MKSQUASHFS $TMPROOTDIR $CURDIR/mtd_root.bin -comp lzma -b 256k -all-root
-echo "PAD 0xa20000 $CURDIR/mtd_root.bin $CURDIR/mtd_root.pad.bin"
-$PAD 0xa20000 $CURDIR/mtd_root.bin $CURDIR/mtd_root.pad.bin
+# Size 0xb40000
+echo "MKSQUASHFS $TMPROOTDIR $CURDIR/mtd_root.bin -comp gzip -all-root"
+$MKSQUASHFS $TMPROOTDIR $CURDIR/mtd_root.bin -comp gzip -b 256k -all-root
+echo "PAD 0xb40000 $CURDIR/mtd_root.bin $CURDIR/mtd_root.pad.bin"
+$PAD 0xb40000 $CURDIR/mtd_root.bin $CURDIR/mtd_root.pad.bin
 
 # --- VAR ---
-# Size 0x400000
-echo "MKFSJFFS2 -qUfv -p0x4100000 -e0x10000 -r $TMPVARDIR -o $CURDIR/mtd_var.bin"
-$MKFSJFFS2 -qUfv -p0x400000 -e0x10000 -r $TMPVARDIR -o $CURDIR/mtd_var.bin > /dev/null
+# Size 0x2e0000
+echo "MKFSJFFS2 -qUfv -p0x2e00000 -e0x10000 -r $TMPVARDIR -o $CURDIR/mtd_var.bin"
+$MKFSJFFS2 -qUfv -p0x2e0000 -e0x10000 -r $TMPVARDIR -o $CURDIR/mtd_var.bin > /dev/null
 echo "SUMTOOL -v -p -e 0x10000 -i $CURDIR/mtd_var.bin -o $CURDIR/mtd_var.sum.bin"
 $SUMTOOL -v -p -e 0x10000 -i $CURDIR/mtd_var.bin -o $CURDIR/mtd_var.sum.bin > /dev/null
-echo "$PAD 0x410000 $CURDIR/mtd_var.sum.bin $CURDIR/mtd_var.sum.pad.bin"
-$PAD 0x400000 $CURDIR/mtd_var.sum.bin $CURDIR/mtd_var.sum.pad.bin > /dev/null
+echo "$PAD 0x2e0000 $CURDIR/mtd_var.sum.bin $CURDIR/mtd_var.sum.pad.bin"
+$PAD 0x2e0000 $CURDIR/mtd_var.sum.bin $CURDIR/mtd_var.sum.pad.bin > /dev/null
 
 # --- update.img ---
 #Merge all 3 together
@@ -65,19 +65,19 @@ rm -f $CURDIR/mtd_var.sum.bin
 SIZE=`stat mtd_kernel.pad.bin -t --format %s`
 SIZE=`printf "0x%x" $SIZE`
 if [[ $SIZE > "0x1a0000" ]]; then
-  echo "KERNEL TO BIG. $SIZE instead of 0x1a0000" > /dev/stderr
+  echo -e "\e[31mKERNEL TO BIG. $SIZE instead of 0x1a0000\e[0m" > /dev/stderr
 fi
 
 SIZE=`stat mtd_root.pad.bin -t --format %s`
 SIZE=`printf "0x%x" $SIZE`
-if [[ $SIZE > "0xa20000" ]]; then
-  echo "ROOT TO BIG. $SIZE instead of 0xa20000" > /dev/stderr
+if [[ $SIZE > "0xb40000" ]]; then
+  echo -e "\e[31mROOT TO BIG. $SIZE instead of 0xa20000\e[0m" > /dev/stderr
 fi
 
 SIZE=`stat mtd_var.sum.pad.bin -t --format %s`
 SIZE=`printf "0x%x" $SIZE`
-if [[ $SIZE > "0x400000" ]]; then
-  echo "VAR TO BIG. $SIZE instead of 0x400000" > /dev/stderr
+if [[ $SIZE > "0x2e0000" ]]; then
+  echo -e "\e[31mVAR TO BIG. $SIZE instead of 0x400000\e[0m" > /dev/stderr
 fi
 
 rm -f $CURDIR/mtd_kernel.pad.bin

720a94fd01e666d1e45bb42e4abbd2068854b7b6
BPanther <bpanther_ts@hotmail.com>
2013-03-01 00:48:47 +0100

    small change flash script

--- tdt/flash/at7500/scripts/flash_part_w_fw.sh
+++ tdt/flash/at7500/scripts/flash_part_w_fw.sh
@@ -25,7 +25,8 @@ HOST=`cat $TMPEXTDIR/etc/hostname`
 [ -d $CURDIR/../../cvs/apps/libstb-hal-exp ] && HAL_REV=_HAL-rev`cd $CURDIR/../../cvs/apps/libstb-hal-exp && git log | grep "^commit" | wc -l`-exp || HAL_REV=_HAL-rev`cd $CURDIR/../../cvs/apps/libstb-hal && git log | grep "^commit" | wc -l`
 [ -d $CURDIR/../../cvs/apps/neutrino-mp-exp ] && NMP_REV=_NMP-rev`cd $CURDIR/../../cvs/apps/neutrino-mp-exp && git log | grep "^commit" | wc -l`-exp || NMP_REV=_NMP-rev`cd $CURDIR/../../cvs/apps/neutrino-mp && git log | grep "^commit" | wc -l`
 gitversion="_BASE-rev`(cd $CURDIR/../../ && git log | grep "^commit" | wc -l)`$HAL_REV$NMP_REV"
-OUTFILE=$OUTDIR/update_w_fw.ird_$HOST$gitversion
+OUTFILE=$OUTDIR/update_w_fw.ird
+OUTFILE_Z=$OUTDIR/$HOST$gitversion
 
 if [ ! -e $OUTDIR ]; then
   mkdir $OUTDIR
@@ -36,7 +37,7 @@ if [ -e $OUTFILE ]; then
   rm -f $OUTFILE.md5
 fi
 
-cp $TMPKERNELDIR/uImage $CURDIR/uImage
+cp $TMPKERNELDIR/uImage $OUTDIR/uImage.bin
 
 # Create a jffs2 partition for fw's
 # Size 6.875  MB = -p0x6E0000
@@ -46,19 +47,19 @@ cp $TMPKERNELDIR/uImage $CURDIR/uImage
 # ./fw
 # ./fw/audio.elf
 # ./fw/video.elf
-echo "MKFSJFFS2 -qUfv -p0x6E0000 -e0x20000 -r $TMPFWDIR -o $CURDIR/mtd_fw.bin"
-$MKFSJFFS2 -qUfv -p0x6E0000 -e0x20000 -r $TMPFWDIR -o $CURDIR/mtd_fw.bin > /dev/null
-echo "SUMTOOL -v -p -e 0x20000 -i $CURDIR/mtd_fw.bin -o $CURDIR/mtd_fw.sum.bin"
-$SUMTOOL -v -p -e 0x20000 -i $CURDIR/mtd_fw.bin -o $CURDIR/mtd_fw.sum.bin > /dev/null
-echo "PAD 0x6E0000 $CURDIR/mtd_fw.sum.bin $CURDIR/mtd_fw.sum.pad.bin"
-$PAD 0x6E0000 $CURDIR/mtd_fw.sum.bin $CURDIR/mtd_fw.sum.pad.bin
+echo "MKFSJFFS2 -qUfv -p0x6E0000 -e0x20000 -r $TMPFWDIR -o $OUTDIR/mtd_fw.bin"
+$MKFSJFFS2 -qUfv -p0x6E0000 -e0x20000 -r $TMPFWDIR -o $OUTDIR/mtd_fw.bin > /dev/null
+echo "SUMTOOL -v -p -e 0x20000 -i $OUTDIR/mtd_fw.bin -o $OUTDIR/mtd_fw.sum.bin"
+$SUMTOOL -v -p -e 0x20000 -i $OUTDIR/mtd_fw.bin -o $OUTDIR/mtd_fw.sum.bin > /dev/null
+echo "PAD 0x6E0000 $OUTDIR/mtd_fw.sum.bin $OUTDIR/mtd_fw.sum.pad.bin"
+$PAD 0x6E0000 $OUTDIR/mtd_fw.sum.bin $OUTDIR/mtd_fw.sum.pad.bin
 
 # Create a jffs2 partition for ext
 # 0x01220000 - 0x01DFFFFF (11.875 MB)
 # Should be p0xBE0000 but due to a bug in stock uboot the size had to be decreased
-$MKFSJFFS2 -qUfv -p0xBA0000 -e0x20000 -r $TMPEXTDIR -o $CURDIR/mtd_ext.bin
-$SUMTOOL -v -p -e 0x20000 -i $CURDIR/mtd_ext.bin -o $CURDIR/mtd_ext.sum.bin
-$PAD 0xBA0000 $CURDIR/mtd_ext.sum.bin $CURDIR/mtd_ext.sum.pad.bin
+$MKFSJFFS2 -qUfv -p0xBA0000 -e0x20000 -r $TMPEXTDIR -o $OUTDIR/mtd_ext.bin
+$SUMTOOL -v -p -e 0x20000 -i $OUTDIR/mtd_ext.bin -o $OUTDIR/mtd_ext.sum.bin
+$PAD 0xBA0000 $OUTDIR/mtd_ext.sum.bin $OUTDIR/mtd_ext.sum.pad.bin
 
 # Create a jffs2 partition for root
 # Size 30     MB = -p0x1E00000
@@ -68,28 +69,31 @@ $PAD 0xBA0000 $CURDIR/mtd_ext.sum.bin $CURDIR/mtd_ext.sum.pad.bin
 # ./release
 # ./release/etc
 # ./release/usr
-echo "MKFSJFFS2 -qUfv -p0x1E00000 -e0x20000 -r $TMPROOTDIR -o $CURDIR/mtd_root.bin"
-$MKFSJFFS2 -qUfv -p0x1E00000 -e0x20000 -r $TMPROOTDIR -o $CURDIR/mtd_root.bin > /dev/null
-echo "SUMTOOL -v -p -e 0x20000 -i $CURDIR/mtd_root.bin -o $CURDIR/mtd_root.sum.bin"
-$SUMTOOL -v -p -e 0x20000 -i $CURDIR/mtd_root.bin -o $CURDIR/mtd_root.sum.bin > /dev/null
-echo "PAD 0x1E00000 $CURDIR/mtd_root.sum.bin $CURDIR/mtd_root.sum.pad.bin"
-$PAD 0x1E00000 $CURDIR/mtd_root.sum.bin $CURDIR/mtd_root.sum.pad.bin
+echo "MKFSJFFS2 -qUfv -p0x1E00000 -e0x20000 -r $TMPROOTDIR -o $OUTDIR/mtd_root.bin"
+$MKFSJFFS2 -qUfv -p0x1E00000 -e0x20000 -r $TMPROOTDIR -o $OUTDIR/mtd_root.bin > /dev/null
+echo "SUMTOOL -v -p -e 0x20000 -i $OUTDIR/mtd_root.bin -o $OUTDIR/mtd_root.sum.bin"
+$SUMTOOL -v -p -e 0x20000 -i $OUTDIR/mtd_root.bin -o $OUTDIR/mtd_root.sum.bin > /dev/null
+echo "PAD 0x1E00000 $OUTDIR/mtd_root.sum.bin $OUTDIR/mtd_root.sum.pad.bin"
+$PAD 0x1E00000 $OUTDIR/mtd_root.sum.bin $OUTDIR/mtd_root.sum.pad.bin
 
 # Create a fortis signed update file for fw's
 # Note: -g is a workaround which will be removed as soon as the missing conf partition is found
 # Note: -e could be used as a extension partition but at the moment we dont use it
-echo "FUP -ce $OUTFILE -k $CURDIR/uImage -f $CURDIR/mtd_fw.sum.pad.bin -g foo -e foo -r $CURDIR/mtd_root.sum.pad.bin"
-$FUP -ce $OUTFILE -k $CURDIR/uImage -f $CURDIR/mtd_fw.sum.pad.bin -r $CURDIR/mtd_root.sum.pad.bin -g foo -e $CURDIR/mtd_ext.sum.pad.bin
+echo "FUP -ce $OUTFILE -k $OUTDIR/uImage.bin -f $OUTDIR/mtd_fw.sum.pad.bin -g foo -e foo -r $OUTDIR/mtd_root.sum.pad.bin"
+$FUP -ce $OUTFILE -k $OUTDIR/uImage.bin -f $OUTDIR/mtd_fw.sum.pad.bin -r $OUTDIR/mtd_root.sum.pad.bin -g foo -e $OUTDIR/mtd_ext.sum.pad.bin
 
-rm -f $CURDIR/uImage
-rm -f $CURDIR/mtd_fw.bin
-rm -f $CURDIR/mtd_fw.sum.bin
-rm -f $CURDIR/mtd_fw.sum.pad.bin
-rm -f $CURDIR/mtd_root.bin
-rm -f $CURDIR/mtd_root.sum.bin
-rm -f $CURDIR/mtd_root.sum.pad.bin
-rm -f $CURDIR/mtd_ext.bin
-rm -f $CURDIR/mtd_ext.sum.bin
-rm -f $CURDIR/mtd_ext.sum.pad.bin
+rm -f $OUTDIR/uImage.bin
+rm -f $OUTDIR/mtd_fw.bin
+rm -f $OUTDIR/mtd_fw.sum.bin
+rm -f $OUTDIR/mtd_fw.sum.pad.bin
+rm -f $OUTDIR/mtd_root.bin
+rm -f $OUTDIR/mtd_root.sum.bin
+rm -f $OUTDIR/mtd_root.sum.pad.bin
+rm -f $OUTDIR/mtd_ext.bin
+rm -f $OUTDIR/mtd_ext.sum.bin
+rm -f $OUTDIR/mtd_ext.sum.pad.bin
 
-zip -j $OUTFILE.zip $OUTFILE
+md5sum -b $OUTFILE | awk -F' ' '{print $1}' > $OUTFILE.md5
+zip -j $OUTFILE_Z.zip $OUTFILE $OUTFILE.md5
+rm -f $OUTFILE
+rm -f $OUTFILE.md5

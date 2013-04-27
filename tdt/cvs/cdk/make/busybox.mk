cba7cb826b248022b2ef8694467b40d5871fd7a9
max10 <max_10@gmx.de>
2013-04-27 21:19:59 +0200

    update busybox.mk

--- tdt/cvs/cdk/make/busybox.mk
+++ tdt/cvs/cdk/make/busybox.mk
@@ -1,27 +1,19 @@
 #
 # busybox
 #
-$(DEPDIR)/busybox.do_prepare: @DEPENDS_busybox@
+$(DEPDIR)/busybox: bootstrap @DEPENDS_busybox@ $(buildprefix)/Patches/busybox.config$(if $(UFS912)$(UFS913)$(SPARK)$(SPARK7162),_nandwrite)
 	@PREPARE_busybox@
 	cd @DIR_busybox@ && \
 		patch -p1 < ../Patches/busybox-1.21.0-mdev.patch && \
 		patch -p1 < ../Patches/busybox-1.21.0-platform.patch && \
-		patch -p1 < ../Patches/busybox-1.21.0-xz.patch
-	touch $@
-
-$(DEPDIR)/busybox.do_compile: bootstrap $(DEPDIR)/busybox.do_prepare Patches/busybox.config$(if $(UFS912)$(UFS913)$(SPARK)$(SPARK7162),_nandwrite) | $(DEPDIR)/$(GLIBC_DEV)
-	cd @DIR_busybox@ && \
-		$(INSTALL) -m644 ../$(lastword $^) .config && \
-		$(MAKE) all \
-			CROSS_COMPILE=$(target)- \
-			CFLAGS_EXTRA="$(TARGET_CFLAGS)"
-	touch $@
-
-$(DEPDIR)/busybox: \
-$(DEPDIR)/%busybox: $(DEPDIR)/busybox.do_compile
+		patch -p1 < ../Patches/busybox-1.21.0-xz.patch && \
+		$(INSTALL) -m644 $(lastword $^) .config && \
+		sed -i -e 's#^CONFIG_PREFIX.*#CONFIG_PREFIX="$(targetprefix)"#' .config
 	cd @DIR_busybox@ && \
 		export CROSS_COMPILE=$(target)- && \
+		$(MAKE) all \
+			CROSS_COMPILE=$(target)- \
+			CONFIG_EXTRA_CFLAGS="$(TARGET_CFLAGS)" && \
 		@INSTALL_busybox@
 #	@DISTCLEANUP_busybox@
-	@[ "x$*" = "x" ] && touch $@ || true
-	@TUXBOX_YAUD_CUSTOMIZE@
+	touch $@

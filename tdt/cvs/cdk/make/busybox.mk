7428b2873d8e73bd290103744f9552b9b44a0e55
BPanther <bpanther_ts@hotmail.com>
2014-01-11 11:50:55 +0100

    busybox update patches

--- tdt/cvs/cdk/make/busybox.mk
+++ tdt/cvs/cdk/make/busybox.mk
@@ -4,6 +4,10 @@
 $(DEPDIR)/busybox: bootstrap @DEPENDS_busybox@ $(buildprefix)/Patches/busybox.config$(if $(UFS912)$(UFS913)$(SPARK)$(SPARK7162),_nandwrite)
 	@PREPARE_busybox@
 	cd @DIR_busybox@ && \
+		patch -p1 < ../Patches/busybox-1.22.0-find.patch && \
+		patch -p1 < ../Patches/busybox-1.22.0-grep.patch && \
+		patch -p1 < ../Patches/busybox-1.22.0-lineedit.patch && \
+		patch -p1 < ../Patches/busybox-1.22.0-ntpd.patch && \
 		$(INSTALL) -m644 $(lastword $^) .config && \
 		sed -i -e 's#^CONFIG_PREFIX.*#CONFIG_PREFIX="$(targetprefix)"#' .config
 	cd @DIR_busybox@ && \

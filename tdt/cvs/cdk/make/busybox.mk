cf6429b49773f111c70f43525ed52f7425be2ca6
BPanther <bpanther_ts@hotmail.com>
2014-03-19 06:53:13 +0100

    busybox patch added

--- tdt/cvs/cdk/make/busybox.mk
+++ tdt/cvs/cdk/make/busybox.mk
@@ -7,6 +7,7 @@ $(DEPDIR)/busybox: bootstrap @DEPENDS_busybox@ $(buildprefix)/Patches/busybox.co
 		patch -p1 < ../Patches/busybox-1.22.1-ash.patch && \
 		patch -p1 < ../Patches/busybox-1.22.1-date.patch && \
 		patch -p1 < ../Patches/busybox-1.22.1-iplink.patch && \
+		patch -p1 < ../Patches/busybox-1.22.1-nc.patch && \
 		$(INSTALL) -m644 $(lastword $^) .config && \
 		sed -i -e 's#^CONFIG_PREFIX.*#CONFIG_PREFIX="$(targetprefix)"#' .config
 	cd @DIR_busybox@ && \

1d4a8636ecfa88f2a2861755ac92733a8456e8d7
max10 <max_10@gmx.de>
2014-03-23 16:53:15 +0100

    version bump util-linux-2.24

--- tdt/cvs/cdk/make/contrib-apps.mk
+++ tdt/cvs/cdk/make/contrib-apps.mk
@@ -172,6 +172,7 @@ $(DEPDIR)/utillinux: bootstrap libz ncurses-dev @DEPENDS_utillinux@
 			--disable-libblkid \
 			--disable-libmount \
 			--disable-bash-completion \
+			--without-ncurses \
 			--disable-wall && \
 		$(MAKE) ARCH=sh4 && \
 		@INSTALL_utillinux@

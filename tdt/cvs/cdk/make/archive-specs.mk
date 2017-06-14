670c945826b380c9b63d98005f0e8e9221a5d6e1
BPanther <bpanther_ts@hotmail.com>
2017-06-15 01:23:32 +0200

    update stlinux.com url

--- tdt/cvs/cdk/make/archive-specs.mk
+++ tdt/cvs/cdk/make/archive-specs.mk
@@ -1,12 +1,12 @@
-URL3=ftp://ftp.stlinux.com/pub/stlinux/2.3/STLinux/sh4
-URL3U=ftp://ftp.stlinux.com/pub/stlinux/2.3/updates/RPMS/sh4
-URL4=ftp://ftp.stlinux.com/pub/stlinux/2.4/STLinux/sh4
-URL4U=ftp://ftp.stlinux.com/pub/stlinux/2.4/updates/RPMS/sh4
+URL3=http://archive.stlinux.com/stlinux/2.3/STLinux/sh4
+URL3U=http://archive.stlinux.com/stlinux/2.3/updates/RPMS/sh4
+URL4=http://archive.stlinux.com/stlinux/2.4/STLinux/sh4
+URL4U=http://archive.stlinux.com/stlinux/2.4/updates/RPMS/sh4
 
-URL3S=ftp://ftp.stlinux.com/pub/stlinux/2.3/SRPMS
-URL3SU=ftp://ftp.stlinux.com/pub/stlinux/2.3/updates/SRPMS
-URL4S=ftp://ftp.stlinux.com/pub/stlinux/2.4/SRPMS
-URL4SU=ftp://ftp.stlinux.com/pub/stlinux/2.4/updates/SRPMS
+URL3S=http://archive.stlinux.com/stlinux/2.3/SRPMS
+URL3SU=http://archive.stlinux.com/stlinux/2.3/updates/SRPMS
+URL4S=http://archive.stlinux.com/stlinux/2.4/SRPMS
+URL4SU=http://archive.stlinux.com/stlinux/2.4/updates/SRPMS
 
 $(archivedir)/stlinux23-sh4-%.sh4.rpm:
 	[ ! -f $(archivedir)/$(notdir $@) ] && \
